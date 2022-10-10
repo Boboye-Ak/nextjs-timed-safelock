d9e4d24c2295fc6c5cd355498ae243713582de2b
Boboye-Ak <boboye771@gmail.com>
2022-10-10 08:27:58 +0100

    Installed react-moralis, moralis, web3uikit and made moralis provider and notification provider

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const safelockABI = require("./SafelockABI.json").abi;
+const safelockFactoryABI = require("./SafelockFactoryABI.json").abi;
+const safelockFactoryAddresses = require("./SafelockFactoryAddress.json");
+
+module.exports = { safelockABI, safelockFactoryABI, safelockFactoryAddresses };
