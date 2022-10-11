fc85487031e08e0848dac53397529fd97feb712b
Boboye-Ak <boboye771@gmail.com>
2022-10-11 20:00:15 +0100

    Started building timer component

---
+++ components/CountdownTimer.js
@@ -0,0 +1,60 @@
+import { useState } from "react"
+
+const CountdownTimer = ({ startTime }) => {
+    const [timerDays, setTimerDays] = useState()
+    const [timerHours, setTimerHours] = useState()
+    const [timerMinutes, setTimerMinutes] = useState()
+    const [timerSeconds, setTimerSeconds] = useState()
+
+    let interval;
+    return (
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div>88</div>
+                <small>days</small>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div>88</div>
+                <small>hours</small>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div>88</div>
+                <small>minutes</small>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div>88</div>
+                <small>seconds</small>
+            </div>
+        </div>
+    )
+}
+
+export default CountdownTimer
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-
 import Image from "next/image"
+import CountdownTimer from "../components/CountdownTimer"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
     return (
         <div className={styles.container}>
-Hello world
+            <CountdownTimer startTime={18400} />
         </div>
     )
 }
