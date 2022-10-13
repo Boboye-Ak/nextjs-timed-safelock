4a187e76d48270e29885a1e360f4dc1a8ff98666
Boboye-Ak <boboye771@gmail.com>
2022-10-13 09:17:51 +0100

    Implemented getSafes in frontend

--- components/Safelock.js
+++ components/Safelock.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useMoralis, useWeb3Contract } from "react-moralis"
 import { safelockFactoryAddresses, safelockFactoryABI, safelockABI } from "../constants"
 
@@ -7,17 +7,45 @@ const Safelock = ({ mySafelockId, mySafelockAddress }) => {
     const chainId = parseInt(chainIdHex)
     const safelockFactoryAddress =
         chainId in safelockFactoryAddresses ? safelockFactoryAddresses[chainId][0] : null
-    const updateUI = async () => {}
+
+    const [safes, setSafes] = useState([])
+    //Web3 functions
+    const {
+        runContractFunction: getSafes,
+        isFetching: getMySafelockIdIsFetching,
+        isLoading: getMySafelockIdIsLoading,
+    } = useWeb3Contract({
+        abi: safelockABI,
+        contractAddress: mySafelockAddress,
+        functionName: "getSafes",
+        params: {},
+    })
+
+    //Web2 Functions
+    const updateUI = async () => {
+        const safesFromCall = await getSafes()
+        console.log(safesFromCall)
+        setSafes(safesFromCall)
+    }
 
     useEffect(() => {
-        if(isWeb3Enabled, mySafelockAddress){
-            
+        if ((isWeb3Enabled, mySafelockAddress)) {
+            updateUI()
         }
     }, [mySafelockAddress, isWeb3Enabled])
     return (
         <div>
             My SafelockId:{mySafelockId}
             My Safelock Address:{mySafelockAddress}
+            My Safes:
+            {safes.map((safe) => {
+                return (
+                    <div>
+                        amount: {safe.amount?.toString()}
+                        Time Length:{safe.timeLength?.toString()}
+                    </div>
+                )
+            })}
         </div>
     )
 }
